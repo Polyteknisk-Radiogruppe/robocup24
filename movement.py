2acb5647a8e9258ffeaa106f4272fe8cc0a2ca35
artur <arturbarcij1@gmail.com>
2024-01-24 22:11:59 +0100

    movement

---
+++ movement.py
@@ -0,0 +1,23 @@
+# THIS FILE IS FOR ROBOT MOVEMENT
+def go():
+    print("go")
+
+
+def catch():
+    print("catch")
+
+
+def grab():
+    print("grab")
+
+
+def drop():
+    print("drop")
+
+
+def push():
+    print("push")
+
+
+def turn():
+    print("turn")
